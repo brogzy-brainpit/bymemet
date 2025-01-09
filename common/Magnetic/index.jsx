5eaedf0e437501dfc30ffc8b1704f8271798f5e0
brainpit <dangabarin2020@gmail.com>
2025-01-09 17:56:31 +0100

    first commit

--- common/Magnetic/index.jsx
+++ common/Magnetic/index.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef } from 'react'
 import gsap from 'gsap';
 
-export default function <i></i>ndex({children}) {
+export default function index({children}) {
     const magnetic = useRef(null);
 
     useEffect( () => {
