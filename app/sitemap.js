0de59a9b6b34f358782fd933d854d88e9d1cecbf
brainpit <dangabarin2020@gmail.com>
2025-03-04 11:26:12 +0100

    first commit

--- app/sitemap.js
+++ app/sitemap.js
@@ -22,30 +22,43 @@ export default  async function sitemap() {
   const postEntries = blogs.map(({ id,slug,createdAt }) => ({
     url: `${process.env.NEXT_PUBLIC_BASE_URL}/blogs/${slug}`,
     lastModified: new Date(createdAt),
+    changeFrequency:"monthly",
+    priority:"0.8",
   }));
 
   return [
     {
       url: `${process.env.NEXT_PUBLIC_BASE_URL}/`,
       lastModified: new Date(),
+      changeFrequency:"monthly",
+      priority:"1",
     },{
       url: `${process.env.NEXT_PUBLIC_BASE_URL}/about`,
       lastModified: new Date(),
+      changeFrequency:"yearly",
+      priority:"0.5",
+
     },
     {
       url: `${process.env.NEXT_PUBLIC_BASE_URL}/works`,
       lastModified: new Date(),
+      changeFrequency:"weekly",
+      priority:"1",
     }, {
       url: `${process.env.NEXT_PUBLIC_BASE_URL}/blogs`,
       lastModified: new Date(),
+      changeFrequency:"weekly",
+      priority:"1",
     },
     {
       url: `${process.env.NEXT_PUBLIC_BASE_URL}/contact`,
       lastModified: new Date(),
+      changeFrequency:"yearly",
+      priority:"1",
     },
       ...postEntries, // Add all post URLs to the sitemap
     ]
 }
 
 // Revalidation period of 2 days (48 hours)
-export const revalidate = 60 ;  // 2 days
+export const revalidate = 60 * 60 * 24;   // Revalidate the page every 24 hours
